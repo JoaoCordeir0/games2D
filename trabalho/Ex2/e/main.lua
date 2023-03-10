19a3de05bd98b2c8d87ba86d9819b491bf7ce4f7
Joao.Cordeiro <joaocordeiro2134@gmail.com>
2023-03-10 16:07:03 -0300

    finish

--- trabalho/Ex2/e/main.lua
+++ trabalho/Ex2/e/main.lua
@@ -3,5 +3,38 @@ function love.load()
 end
 
 function love.draw()
-    
+    love.graphics.line(
+        love.math.newBezierCurve(
+            150, 150,                      
+            200, -50,            
+            250, 150            
+        ):render()
+    )
+    love.graphics.line(
+        love.math.newBezierCurve(
+            250, 150,
+            450, 200,
+            250, 250
+        ):render()
+    )
+    love.graphics.line(
+        love.math.newBezierCurve(
+            250, 250,
+            200, 450,
+            150, 250
+        ):render()
+    )
+    love.graphics.line(
+        love.math.newBezierCurve(
+            150, 150,
+            -50, 200,
+            150, 250
+        ):render()
+    )
+    love.graphics.circle(
+        'line',
+        200,
+        200,
+        50
+    )
 end
