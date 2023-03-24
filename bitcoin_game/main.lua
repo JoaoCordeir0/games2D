f07f3514fff933562cc9902fadcde0104c3bcf2b
joao victor cordeiro <joaocordeiro2134@gmail.com>
2023-03-24 18:10:35 -0300

    att game bitcoin

--- bitcoin_game/main.lua
+++ bitcoin_game/main.lua
@@ -3,21 +3,24 @@
 local LG = love.graphics
 local KD = love.keyboard.isDown
 
+-- Biblioteca para serialização dos dados para salvar e carregar
+lume = require('lume')
+
 -- Variáveis globais: Estrutura do jogo
 character = {} -- Personagem
 wallet = {} -- Carteira
 wingame = false
 loadgame = false
 
-function love.load()
-    LG.setFont(LG.newFont(14))    
+function love.load()    
     Start()
 end
 
 function love.draw()
     -- Instruções na tecla
+    LG.setFont(LG.newFont(14))  
     LG.print("Teclas de controle: [S]alvar, [C]arregar e [R]einiciar", 5, 5)
-    LG.printf("Current FPS: " .. tostring(love.timer.getFPS( )), 5, 5, LG.getWidth() - 10, "right")
+    LG.printf("FPS: " .. tostring(love.timer.getFPS( )), 5, 5, LG.getWidth() - 10, "right")
     
     LG.draw(
         character.img,
@@ -100,6 +103,25 @@ function love.update(dt)
     end
 end
 
+-- Funções de salvar, recarregar e reiniciar
+function love.keypressed(bind)
+    if bind == 's' then
+        print("Salvar")
+        Save()
+    end
+    if bind == 'c' then
+        print("Carregar")
+        loadgame = true
+        Start()
+    end
+    if bind == 'r' then
+        print("Reiniciar")
+        loadgame = false
+        love.filesystem.remove("savegame.txt")
+        love.event.quit("restart")
+    end
+end
+
 -- Funções personalizadas
 function Start()
     character = {x = 30, y = 50, width = 50, scale = 1, img = nill, speed = 200}
@@ -107,14 +129,18 @@ function Start()
 
     wallet = {}
     math.randomseed(os.time())
-    for c = 1, 25 do 
-        table.insert(wallet, {
-            x = math.random(50, LG.getWidth() - 50),
-            y = math.random(50, LG.getHeight() - 50),
-            img = LG.newImage("bitcoin.png"),
-            width = 25
-        })
-    end
+    if not loadgame then
+        for c = 1, 25 do 
+            table.insert(wallet, {
+                x = math.random(50, LG.getWidth() - 50),
+                y = math.random(50, LG.getHeight() - 50),
+                img = LG.newImage("bitcoin.png"),
+                width = 25
+            })
+        end
+    else 
+        Reload()
+    end 
 end
 
 function haveCollision(p1, p2)
@@ -123,4 +149,59 @@ function haveCollision(p1, p2)
     
     -- Verifica a colisão
     return distance < (p1.width + p2.width) / 2
+end
+
+-- Funções de persistência
+function Save()
+    gameData = {}
+    gameData.character = {
+        x = character.x,
+        y = character.y,
+        width = character.width,
+        scale = character.scale
+    }
+
+    gameData.wallet = {}
+    for c, actual in ipairs(wallet) do
+        gameData.wallet[c] = {
+            x = actual.x,
+            y = actual.y,
+            width = actual.width
+        }
+    end
+
+    -- Apenas para verificação
+    print(gameData)
+
+    -- Serialização dos dados para salvar
+    serialize = lume.serialize(gameData)
+
+    -- Local padrão de salvamento do LOVE: %appdata%\LOVE\
+    love.filesystem.write('savegame.txt', serialize)
+end
+
+function Reload()
+    if love.filesystem.getInfo('savegame.txt') then
+        file = love.filesystem.read('savegame.txt')
+
+        -- Desearilizar os dados salvos
+        gameData = lume.deserialize(file)
+
+        -- Carrega o personagem
+        character.x = gameData.character.x
+        character.y = gameData.character.y
+        character.width = gameData.character.width
+        character.scale= gameData.character.scale
+
+        -- Carregar a carteira com as moedas restantes
+        wallet = {}
+        for c, actual in ipairs(gameData.wallet) do
+            wallet[c] = {
+                x = actual.x,
+                y = actual.y,
+                width = actual.width,
+                img = love.graphics.newImage('bitcoin.png')
+            }
+        end
+    end
 end
